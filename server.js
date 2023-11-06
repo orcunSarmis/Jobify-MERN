f1fbcb212ae8f74a2fa9dbf7855d9ced1538eb55
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-11-06 21:46:00 +1000

    adding env and morgan

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-import express from'express'
-const app = express()
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from'express';
+const app = express();
+import morgan from 'morgan';
+
+if(process.env.NODE_ENV=== 'develpment') {
+	app.use(morgan('dev'));
+}
+
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('amk');
+	res.send('mad it');
+});
+
+app.post('/', (req, res) => {
+	console.log(req);
+
+	res.json({ message: 'Data received', data: req.body });
 });
 
 app.listen(5100, () => {
